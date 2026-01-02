3e7dc084556f291087db8650ba3fb4a52f744105
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-02 15:22:12 +0100

    Lesson 7

--- angular-testing-course/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course/src/app/courses/services/calculator.service.spec.ts
@@ -4,18 +4,24 @@ import { LoggerService } from "./logger.service";
 describe('CalculatorService', () => {
   it('Should add two numbers', () => {
     // Given
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+    spyOn(logger, 'log');
+    const calculator = new CalculatorService(logger);
     // When
     const result = calculator.add(2, 3);
     // Then
     expect(result).toBe(5);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two numbers', () => {
     // Given
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = jasmine.createSpyObj<LoggerService>('loggerService', ['log']);
+    // logger.log.and.returnValue();
+    const calculator = new CalculatorService(logger);
     // When
     const result = calculator.subtract(2, 3);
     // Then
     expect(result).toBe(-1, 'Unexpected subtraction result');
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 });
