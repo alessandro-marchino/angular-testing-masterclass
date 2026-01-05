caf9b12b4400228b0647a24681ffddd0dc1c9448
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-05 09:29:22 +0100

    Lesson 21

--- angular-testing-course/src/app/courses/courses-card-list/course-card-list.component.spec.ts
+++ angular-testing-course/src/app/courses/courses-card-list/course-card-list.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { CoursesModule } from "../courses.module";
 import { CoursesCardListComponent } from "./courses-card-list.component";
+import { setupCourses } from "../common/setup-test-data";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(waitForAsync(() =>
     TestBed.configureTestingModule({
@@ -14,15 +18,21 @@ describe('CoursesCardListComponent', () => {
     .then(() => {
       fixture = TestBed.createComponent(CoursesCardListComponent);
       component = fixture.componentInstance;
+      el = fixture.debugElement;
     })
   ));
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
   it('should display the course list', () => {
-    pending();
+    component.courses = setupCourses();
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy();
+    expect(cards.length).toBe(12);
   });
+
   it('should display the first course', () => {
     pending();
   });
