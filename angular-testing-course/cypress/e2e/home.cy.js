28278428e876445e44562a7e52f2e2f0c091cb8f
amarchino <marchino.alessandro@gmail.com>
2026-01-05 23:35:15 +0100

    Lesson 40

--- angular-testing-course/cypress/e2e/home.cy.js
+++ angular-testing-course/cypress/e2e/home.cy.js
@@ -1,32 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept({ method: 'GET', url: '/api/courses' }, { fixture: 'courses.json' }).as('courses');
+  });
+
   it('should display a list of courses', () => {
     cy.visit('/');
+    cy.wait(['@courses']);
     cy.contains('All Courses');
+    cy.get('mat-card').should('have.length', 9);
   })
-
-    // beforeEach(() => {
-
-    //     cy.fixture('courses.json').as("coursesJSON");
-
-    //     cy.server();
-
-    //     cy.route('/api/courses', "@coursesJSON").as("courses");
-
-    //     cy.visit('/');
-
-    // });
-
-    // it('should display a list of courses', () => {
-
-    //     cy.contains("All Courses");
-
-    //     cy.wait('@courses');
-
-    //     cy.get("mat-card").should("have.length", 9);
-
-    // });
 
     // it('should display the advanced courses', () => {
 
