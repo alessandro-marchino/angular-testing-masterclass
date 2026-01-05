afad3e550842206b63c0991a8cf4dc44db7b0bf7
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-05 09:25:22 +0100

    Lesson 20

--- angular-testing-course/src/app/courses/courses-card-list/course-card-list.component.spec.ts
+++ angular-testing-course/src/app/courses/courses-card-list/course-card-list.component.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { CoursesModule } from "../courses.module";
+import { CoursesCardListComponent } from "./courses-card-list.component";
 
 describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() =>
     TestBed.configureTestingModule({
       imports: [ CoursesModule ]
-    });
-  });
+    })
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(CoursesCardListComponent);
+      component = fixture.componentInstance;
+    })
+  ));
 
   it('should create the component', () => {
-    pending();
+    expect(component).toBeTruthy();
   });
   it('should display the course list', () => {
     pending();
