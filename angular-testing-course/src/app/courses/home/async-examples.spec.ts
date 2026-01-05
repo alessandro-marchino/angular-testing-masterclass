e9890b4aae245b73929ae3d28e5740f832ba9cbf
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-05 14:22:16 +0100

    Lesson 30

--- angular-testing-course/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course/src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
+
 fdescribe('Async Testing examples', () => {
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
     let test = false;
@@ -9,5 +11,23 @@ fdescribe('Async Testing examples', () => {
     expect(test).toBeFalsy();
   });
 
+  it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {
+      test = true;
+    }, 1000);
+    expect(test).toBeFalse();
+    tick(1000);
+    expect(test).toBeTrue();
+  }));
 
+  it('Asynchronous test example - setTimeout() with flush', fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {
+      test = true;
+    }, 1000);
+    expect(test).toBeFalse();
+    flush();
+    expect(test).toBeTrue();
+  }));
 });
