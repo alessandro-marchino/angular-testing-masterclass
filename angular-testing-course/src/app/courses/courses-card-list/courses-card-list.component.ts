4f8d6a9bcff5bf9e39a33c4bc886f947b3a8f2b5
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-05 09:15:29 +0100

    Lesson 19

--- angular-testing-course/src/app/courses/courses-card-list/courses-card-list.component.ts
+++ angular-testing-course/src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {Course} from '../model/course';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import {filter, tap} from 'rxjs/operators';
 
@@ -18,25 +18,20 @@ export class CoursesCardListComponent implements OnInit {
   @Output()
   courseEdited = new EventEmitter();
 
-  constructor(private dialog: MatDialog) {
-
-  }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
-
+    // Empty
   }
 
   editCourse(course: Course) {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
 
     dialogRef.afterClosed()
       .pipe(
@@ -44,16 +39,6 @@ export class CoursesCardListComponent implements OnInit {
         tap(() => this.courseEdited.emit())
       )
       .subscribe();
-
   }
 
 }
-
-
-
-
-
-
-
-
-
