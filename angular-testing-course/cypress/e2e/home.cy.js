42e237cceed5b7c50ac7943bb8d474f47cb80355
amarchino <marchino.alessandro@gmail.com>
2026-01-05 23:40:40 +0100

    Lesson 41

--- angular-testing-course/cypress/e2e/home.cy.js
+++ angular-testing-course/cypress/e2e/home.cy.js
@@ -3,49 +3,20 @@
 describe('Home Page', () => {
   beforeEach(() => {
     cy.intercept({ method: 'GET', url: '/api/courses' }, { fixture: 'courses.json' }).as('courses');
+    cy.visit('/');
+    cy.wait(['@courses']);
   });
 
   it('should display a list of courses', () => {
-    cy.visit('/');
-    cy.wait(['@courses']);
     cy.contains('All Courses');
     cy.get('mat-card').should('have.length', 9);
-  })
+  });
 
-    // it('should display the advanced courses', () => {
-
-    //     cy.get('.mat-tab-label').should("have.length", 2);
-
-    //     cy.get('.mat-tab-label').last().click();
-
-    //     cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
-
-    //     cy.get('.mat-tab-body-active .mat-card-title').first()
-    //         .should('contain', "Angular Security Course");
-
-    // });
-
+  it('should display the advanced courses', () => {
+    cy.get('.mat-mdc-tab').should('have.length', 2);
+    cy.get('.mat-mdc-tab').last().click();
+    cy.get('mat-card').should('have.length', 3);
+    cy.get('mat-card-title').first().should('contain', 'Angular Security Course');
+  });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
