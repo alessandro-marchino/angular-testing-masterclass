f9b0daba061541b2820ae9e5990ad5c7ba846e8b
amarchino <marchino.alessandro@gmail.com>
2026-06-02 22:47:53 +0200

    Lesson 7

--- angular-testing-in-depth/src/app/common/calculator.spec.ts
+++ angular-testing-in-depth/src/app/common/calculator.spec.ts
@@ -14,4 +14,15 @@ describe("Vitest Fundamentals", () => {
     expect(spy).toHaveBeenCalledOnce();
     expect(spy).toHaveBeenCalledWith(2, 3);
   });
+
+  it.only("Shows how mocking work", () => {
+    const spy = vi.spyOn(calculator, 'add').mockReturnValue(5);
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenCalledWith(2, 3);
+
+    const result2 = calculator.add(5, 5);
+    expect(result2).not.toBe(10);
+  });
 });
