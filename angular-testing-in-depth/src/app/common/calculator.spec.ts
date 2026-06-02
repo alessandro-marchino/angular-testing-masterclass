7b8f751f633f255d87883fa9a081187c3cd4d431
amarchino <marchino.alessandro@gmail.com>
2026-06-02 22:44:01 +0200

    Lesson 6

--- angular-testing-in-depth/src/app/common/calculator.spec.ts
+++ angular-testing-in-depth/src/app/common/calculator.spec.ts
@@ -1,20 +1,17 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { calculator } from './calculator';
 
 describe("Vitest Fundamentals", () => {
-  it.skipIf(false)("Should add two numbers", () => {
+  it("Should add two numbers", () => {
     const result = calculator.add(2, 3);
     expect(result).toBe(5);
   });
 
-  it.skip("Should add two numbers V2", () => {
+  it("Shows how spies work", () => {
+    const spy = vi.spyOn(calculator, 'add');
     const result = calculator.add(2, 3);
     expect(result).toBe(5);
-  });
-
-  // With .only ou can run only this test
-  it("Should add two numbers V3", () => {
-    const result = calculator.add(2, 3);
-    expect(result).toBe(5);
+    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenCalledWith(2, 3);
   });
 });
