f31a85254a2363a3b678cb4e278e6f90a84469ee
amarchino <marchino.alessandro@gmail.com>
2026-06-04 22:20:10 +0200

    Lesson 28

--- angular-testing-in-depth/src/app/courses/courses.spec.ts
+++ angular-testing-in-depth/src/app/courses/courses.spec.ts
@@ -6,6 +6,8 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { CoursesService } from '../services/courses.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { MOCK_COURSES } from '../testing/testing-data';
+import { By } from '@angular/platform-browser';
 
 describe('CoursesController', () => {
   let component: Courses;
@@ -35,6 +37,20 @@ describe('CoursesController', () => {
     expect(component).toBeDefined();
   });
 
-  it('Should load courses and filtered by category', () => {});
-  it('Should show advanced courses when tab clicked', () => {});
+  it('Should load courses and filter by category', async () => {
+    const req = httpMock.expectOne('/api/courses');
+    req.flush({ payload: MOCK_COURSES });
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    const titles = de.queryAll(By.css('.course-card .card-header'));
+    expect(titles).toHaveLength(1);
+
+    const titleEl = titles[0].nativeElement;
+    expect(titleEl.textContent).toBe('Beginner Course');
+  });
+
+  it.skip('Should show advanced courses when tab clicked', () => {
+
+  });
 });
