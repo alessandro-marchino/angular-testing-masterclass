c45981e392904fc2046636c0a4488bee246a1175
amarchino <marchino.alessandro@gmail.com>
2026-06-06 20:59:05 +0200

    Lesson 57

--- angular-testing-in-depth/src/app/services/course.resolver.spec.ts
+++ angular-testing-in-depth/src/app/services/course.resolver.spec.ts
@@ -1,13 +1,14 @@
-import { beforeEach, describe, vi } from 'vitest';
+import { beforeEach, describe, expect, it, Mocked, vi } from 'vitest';
 import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
 import { CoursePage } from '../course-page/course-page';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { courseResolver } from './course.resolver';
 import { RouterTestingHarness } from '@angular/router/testing';
+import { MOCK_COURSES } from '../testing/testing-data';
 
 describe('CourseResolver', () => {
-  let mockCoursesService: Partial<CoursesService>;
+  let mockCoursesService: Partial<Mocked<CoursesService>>
   let harness: RouterTestingHarness;
 
   beforeEach(async () => {
@@ -29,4 +30,16 @@ describe('CourseResolver', () => {
     }).compileComponents();
     harness = await RouterTestingHarness.create();
   });
+
+  it('Should load correct route by id', async () => {
+    mockCoursesService.findCourseById?.mockResolvedValueOnce(MOCK_COURSES[0]);
+    const component = await harness.navigateByUrl('/courses/1', CoursePage);
+    const router = TestBed.inject(Router);
+    expect(router.url).toBe('/courses/1');
+    expect(mockCoursesService.findCourseById).toHaveBeenCalledOnce();
+    expect(mockCoursesService.findCourseById).toHaveBeenLastCalledWith('1');
+
+    expect(component.course()).toEqual(MOCK_COURSES[0]);
+    expect(harness.routeNativeElement?.textContent).toContain('Beginner Course');
+  })
 });
